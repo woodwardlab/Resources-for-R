29e99383f369a82195f1b744ba2e60955ee6eb2d
nicoleburke <nicole.b.burke@gmail.com>
2019-05-06 15:07:44 -0500

    adding how to use github to the woodward lab repo

--- Adjustments for multiple comparisons/Adjustments for multiple comparisons.R
+++ Adjustments for multiple comparisons/Adjustments for multiple comparisons.R
@@ -4,7 +4,7 @@
 # If you ran 4 tests, then you would make a vector like this
 pvals <- c(pval1, pval2, pval3, pval4)
 
-# This is the code! 
+# This is the code!
 # use this to see more about the fuctions 
 help("p.adjust")
 p.adjust(pvals, method = "fdr", n = length(pvals))
