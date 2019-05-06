1c3fd3811ee6ab4031655638b387b6577238fa9d
nicoleburke <nicole.b.burke@gmail.com>
2019-05-06 14:53:03 -0500

    adding folder for multiple comparisons

---
+++ Adjustments for multiple comparisons/Adjustments for multiple comparisons.R
@@ -0,0 +1,10 @@
+#### adjustments for multiple comparisons 
+
+# Make a vector of your p-values 
+# If you ran 4 tests, then you would make a vector like this
+pvals <- c(pval1, pval2, pval3, pval4)
+
+# This is the code! 
+# use this to see more about the fuctions 
+help("p.adjust")
+p.adjust(pvals, method = "fdr", n = length(pvals))
